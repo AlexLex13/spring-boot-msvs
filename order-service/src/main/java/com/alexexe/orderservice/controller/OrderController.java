0d1c226339695e8a9f6a941847702ac8e56e2531
Alexey <suxovarov.alexey@gmail.com>
2023-03-10 11:50:39 +0300

    add @CircuitBreaker

--- order-service/src/main/java/com/alexexe/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/alexexe/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.alexexe.orderservice.controller;
 
 import com.alexexe.orderservice.dto.OrderRequest;
 import com.alexexe.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +14,13 @@ public class OrderController {
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
         return "Order Places successfully";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return "Oops! Something went wrong, please order after some alter!";
     }
 }
